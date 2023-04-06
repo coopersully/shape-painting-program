0aea779fc030e4d9839c0b439852760cf2dd6d43
Cooper Sullivan <thecoopersully@gmail.com>
2023-04-06 11:58:45 -0500

    Circle extends Oval

--- src/main/java/me/coopersully/shapepainter/shapes/Circle.java
+++ src/main/java/me/coopersully/shapepainter/shapes/Circle.java
@@ -1,24 +1,9 @@
 package me.coopersully.shapepainter.shapes;
 
-import java.awt.*;
+public class Circle extends Oval {
 
-public class Circle extends Shape {
 
-    protected int widthheight = 10;
-
-    public Circle(int x, int y, int widthheight, String color) {
-        super(x, y, color);
-        this.widthheight = widthheight;
+    public Circle(int x, int y, int width, String color) {
+        super(x, y, width, width, color);
     }
-
-    @Override
-    public void draw(Graphics g) {
-        if (g==null)
-            System.out.println("drawing a " + widthheight + " " + color + " circle");
-        else {
-            g.setColor(color);
-            g.drawOval(x, y, widthheight, widthheight);
-        }
-    }
-
 }
